3436461cac1a0abe1e00ebb3b3ff758cd1382e12
ardy <ardywirakusuma16@gmail.com>
2023-10-06 19:48:41 +0800

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['index.html', 'script.js'],
+  content: ['index.html', 'vue.html'],
   theme: {
-    container : {
-      center : true,
+    container: {
+      center: true,
       padding: '16px',
     },
     extend: {
-      colors: {
-        primary : '#6d28d9',
-        secondary : '#64748b',
-        dark : '#0f172a',
+      backgroundImage: {
+        'gambar': "url('/img/5137894.jpg')",
+        'footer-texture': "url('/img/footer-texture.png')",
       },
-      screens : {
-        '2xl' : '1320px',
+      colors: {
+        primary: '#6d28d9',
+        secondary: '#64748b',
+        dark: '#0f172a',
+      },
+      screens: {
+        '2xl': '1320px',
       }
     },
   },
